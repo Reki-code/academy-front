7b683cc62136f2fe4dffa44dd6c5b4908b5fbd3b
Reki-code <weiruiqi@hotmail.com>
2021-02-13 21:57:42 +0800

    add list to Me page

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtAvatar, AtButton } from 'taro-ui'
+import { AtAvatar, AtButton, AtList, AtListItem } from 'taro-ui'
 import Taro from '@tarojs/taro'
 import { Router } from 'tarojs-router'
 
@@ -15,15 +15,38 @@ const Me = () => {
       console.error(e)
     }
   }
-	return (
-		<View>
-			<AtAvatar circle image='https://jdc.jd.com/img/200' />
-			<Text>
-				This is ME!!!
+  return (
+    <View>
+      <AtAvatar circle image='https://jdc.jd.com/img/200' />
+      <Text>
+        Display name
 			</Text>
+      <Text>
+        UID
+      </Text>
+      <AtList>
+        <AtListItem
+          title='我的关注'
+          note='我关注的资料'
+          arrow='right'
+          iconInfo={{ value: 'star' }}
+        />
+        <AtListItem
+          title='我的讨论'
+          note='参加讨论的帖子都在这里'
+          arrow='right'
+          iconInfo={{ value: 'help' }}
+        />
+        <AtListItem
+          title='个人资料'
+          note='用户名等信息'
+          arrow='right'
+          iconInfo={{ value: 'user' }}
+        />
+      </AtList>
       <AtButton onClick={handleLogout}>退出登录</AtButton>
-		</View>
-	)
+    </View>
+  )
 }
 
 export default Me
